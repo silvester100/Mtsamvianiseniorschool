6d30d475be86a4238f4ec24769efaacb337ac5cb
silvester100 <silvestergeorge100@gmail.com>
2025-07-03 12:14:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -47,4 +47,5 @@ app.register_blueprint(developer_bp)
 
 # Main entry point
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
